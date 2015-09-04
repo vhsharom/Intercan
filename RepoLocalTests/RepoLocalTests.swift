8344f461bd35dbce7a3fe2d817e12504909e6486
vhsharom <vhsharo@gmail.com>
2015-09-03 19:27:24 -0500

    Initial Commit

---
+++ RepoLocalTests/RepoLocalTests.swift
@@ -0,0 +1,36 @@
+//
+//  RepoLocalTests.swift
+//  RepoLocalTests
+//
+//  Created by Victor Haro on 03/09/15.
+//  Copyright (c) 2015 code3e. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class RepoLocalTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
